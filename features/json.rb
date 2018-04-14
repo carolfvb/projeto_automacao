987cd202578880660df2adad58ab1e95d3e5c94d
Marcos Barbosa <mpbarbosa_@outlook.com>
2018-04-14 16:53:01 -0300

    Aula 2

---
+++ features/json.rb
@@ -0,0 +1,16 @@
+MASSA = {
+  usuarioValidoCpf: {
+    cpf: FFaker::IdentificationBR.cpf,
+    email: FFaker::Internet.email,
+    telefone: FFaker::PhoneNumberBR.phone_number,
+    telefone2: FFaker::PhoneNumberBR.phone_number
+  },
+  usuarioValidoPj: {
+    cnpj: FFaker::IdentificationBR.cnpj,
+    email: FFaker::Internet.email,
+    telefone: FFaker::PhoneNumberBR.phone_number
+  },
+  skus: {
+    marcas: ['Samsung','iPhone','Lg','Motorola','Lenovo','Nokia']
+  }
+}
--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -1,28 +1,42 @@
-Dado(/^que estou na url "([^"]*)"$/) do |arg1|
- visit(arg1)
+Dado(/^que estou na url "([^"]*)"$/) do |site|
+ visit(URL[site])
 end
 
 
-Entao(/^valido a presença do elemento "([^"]*)"$/) do |arg1|
-   assert_selector(EL[arg1],wait:10)
+Entao(/^valido a presença do elemento "([^"]*)"$/) do |campo|
+   Assercoes.new.valida_existencia_elemento(campo)
 end
 
-  Entao(/^valido a legenda identificacao$/) do
-    assert_text("Identificação")
+Entao(/^valido a legenda "([^"]*)"$/) do |texto|
+  Assercoes.new.valida_existencia_texto(texto)
   end
 
-  Dado(/^preencho o campo "([^"]*)" com "([^"]*)"$/) do |campo, valor|
-    find(EL[campo]).set(valor)
+Dado(/^preencho o campo "([^"]*)" com o item "([^"]*)"$/) do |campo, valor|
+  skus = MASSA[:skus]
+  $produto = skus[:"#{valor}"].sample
+  Common.new.preenche_campo(campo,$produto)
 end
 
 Dado(/^seleciono "([^"]*)"$/) do |campo|
-    find(EL[campo]).click
+    Common.new.clica_elemento(campo)
 end
 
 Dado(/^clico no botao "([^"]*)"$/) do |campo|
-    find(EL[campo]).click
+    Common.new.clica_elemento(campo)
 end
 
 Dado(/^seleciono o campo "([^"]*)" com "([^"]*)"$/) do |campo, valor|
-    find(EL[campo]).select(valor)
+    Common.new.seleciona_elemento(campo,valor)
+end
+
+Dado(/^preencho todos os campos com sucesso$/) do
+  PaginaRegistro.new.preencher_formulario_cpf
+end
+
+Dado(/^preencho todos os campos$/) do
+  PaginaRegistro.new.preencher_formulario_cnpj
+end
+
+Entao(/^devo visualizar o resultado da busca$/) do
+  Assercoes.new.valida_primeiro_item_vitrine($produto)
 end
